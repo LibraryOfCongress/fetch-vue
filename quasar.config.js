406956b22584d8d871086c94471eac96111a5b42
zegle <zegle@loc.gov>
2023-08-29 18:08:50 -0500

    Create Base Vue Frontend from Quasar CLI and setup proper configuration and removes un-needed files/settings

---
+++ quasar.config.js
@@ -0,0 +1,119 @@
+/* eslint-env node */
+
+/*
+ * This file runs in a Node context (it's NOT transpiled by Babel), so use only
+ * the ES6 features that are supported by your Node version. https://node.green/
+ */
+
+// Configuration for your app
+// https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
+
+
+const { configure } = require('quasar/wrappers');
+const path = require('path')
+
+
+module.exports = configure(function (/* ctx */) {
+  return {
+    eslint: {
+      // include: [],
+      // exclude: [],
+      warnings: true,
+      errors: true
+    },
+
+    // https://v2.quasar.dev/quasar-cli/prefetch-feature
+    preFetch: true,
+
+    // app boot file (/src/boot) (this is your main.js)
+    // --> boot files are generated into a "main.js" file
+    // https://v2.quasar.dev/quasar-cli/boot-files
+    boot: [
+      'axios',
+    ],
+
+    // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
+    css: [
+      'app.scss'
+    ],
+
+    // https://github.com/quasarframework/quasar/tree/dev/extras
+    // setup icons packs, external fonts and other external plugins here
+    extras: [
+      // 'ionicons-v4',
+      // 'mdi-v5',
+      // 'fontawesome-v6',
+      // 'eva-icons',
+      // 'themify',
+      // 'line-awesome',
+      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+      'roboto-font', // optional, you are not bound to it
+      'material-icons', // optional, you are not bound to it
+    ],
+
+    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
+    devServer: {
+      open: true, // opens browser window automatically
+      port: 8080
+    },
+
+    // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
+    framework: {
+      config: {},
+      iconSet: 'material-icons', // Quasar icon set
+      lang: 'en-US', // Quasar language pack
+
+      // For special cases outside of where the auto-import strategy can have an impact
+      // (like functional components as one of the examples),
+      // you can manually specify Quasar components/directives to be available everywhere:
+      //
+      // components: [],
+      // directives: [],
+
+      // Quasar plugins
+      plugins: []
+    },
+
+    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
+    build: {
+      target: {
+        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
+        node: 'node16'
+      },
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      },
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      vueOptionsAPI: true,
+      publicPath: '/',
+      // vueDevtools,
+      // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
+      // analyze: true,
+      // env: {},
+      // rawDefine: {}
+      // ignorePublicFolder: true,
+      // minify: false,
+      // polyfillModulePreload: true,
+      // distDir
+      // extendViteConf (viteConf) {},
+      // viteVuePluginOptions: {},
+      // vitePlugins: [
+      //   [ 'package-name', { ..options.. } ]
+      // ]
+    },
+
+    // https://v2.quasar.dev/quasar-cli/developing-pwa/configuring-pwa
+    pwa: {
+      workboxMode: 'generateSW', // or 'injectManifest'
+      injectPwaMetaTags: true,
+      swFilename: 'sw.js',
+      manifestFilename: 'manifest.json',
+      useCredentialsForManifestTag: false,
+      // useFilenameHashes: true,
+      // extendGenerateSWOptions (cfg) {}
+      // extendInjectManifestOptions (cfg) {},
+      // extendManifestJson (json) {}
+      // extendPWACustomSWConf (esbuildConf) {}
+    }
+  }
+});
