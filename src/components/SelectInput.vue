7035a1b1ffacb2b38b45e028559a81369f234a47
zegle <zegle@loc.gov>
2023-12-08 16:10:56 -0600

    FETCH-320: Remove Exposed .env Files in Repo

---
+++ src/components/SelectInput.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-select
+    :dense="currentScreenSize <= 600"
+    outlined
+    v-model="modelValue"
+    :options="localOptions"
+    :option-value="optionValue"
+    :option-label="optionLabel"
+    emit-value
+    map-options
+    use-input
+    hide-selected
+    fill-input
+    input-debounce="0"
+    @filter="filterOptions"
+    class="custom-select full-width"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script setup>
+import { defineModel, ref } from 'vue'
+import { useCurrentScreenSize } from '@/composables/useCurrentScreenSize.js'
+
+// Props
+const modelValue = defineModel()
+const mainProps = defineProps({
+  options: {
+    type: Array,
+    default () {
+      return []
+    },
+    required: true
+  },
+  optionValue: {
+    type: String,
+    default: ''
+  },
+  optionLabel: {
+    type: String,
+    default: ''
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  }
+})
+
+// Compasables
+const { currentScreenSize } = useCurrentScreenSize()
+
+// Local Data
+const localOptions = ref(mainProps.options)
+
+// Logic
+const filterOptions = (val, update) => {
+  update(() => {
+    localOptions.value = mainProps.options.filter(opt => opt[mainProps.optionLabel].toString().toLowerCase().indexOf(val.toLowerCase()) > -1)
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.custom-select {
+  :deep(.q-placeholder) {
+    width: inherit;
+  }
+}
+</style>
