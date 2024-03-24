196fd7642bd3fb9000d255e2b81960211e06f275
Gregory Wolfe <gwolfe@loc.gov>
2024-03-24 15:19:43 -0500

    add helper to refresh and seed db from vue local

--- helper.sh
+++ helper.sh
@@ -8,6 +8,19 @@ web() {
   docker exec -it $1 /bin/sh;
 }
 
+refresh-db() {
+  # Wipe db and build
+  (cd ../fetch-local && exec ./helper.sh wipe-inventory-db);
+  # Give the db a moment to catch its breath
+  sleep 5;
+  # Then re-schema and re-seed
+  (cd ../fetch-local \
+    && export SEED_FAKE_DATA=true \
+    && exec ./helper.sh build-inventory-api \
+    && export SEED_FAKE_DATA=false);
+    # Reset seed arg in case user runs compose elsewhere
+}
+
 build() {
   if [[ "$1" == "local" ]]; then
     ./local-build.sh;
